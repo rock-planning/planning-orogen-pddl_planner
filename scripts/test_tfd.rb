1c4051915dee562eee97d65dd6dc29ee828f7e36
Satia Herfert <satia.herfert@dfki.de>
2014-08-14 15:09:25 +0200

    Test data for TFD.

---
+++ scripts/test_tfd.rb
@@ -0,0 +1,38 @@
+require 'orocos'
+include Orocos
+Orocos.initialize
+
+Orocos.run 'pddl_planner_test', :wait => 5 do 
+
+    planner = TaskContext.get 'pddl_planner'
+
+    planner.planner_name = "TFD"
+    planner.configure
+    planner.start
+
+
+    domain_description = File.read(File.join(File.dirname(__FILE__),"test-data","test_tfd_domain.pddl"))
+    problem = File.read(File.join(File.dirname(__FILE__),"test-data","test_tfd_problem.pddl"))
+
+    if not planner.set_domain_description("test-domain", domain_description)
+        raise "Failed to set domain description"
+    else 
+        puts "Set description: #{domain_description}"
+    end
+
+    candidates = planner.request_plan_candidates(problem)
+    puts "Request plan: #{problem}"
+    
+    puts "Printing plans: "
+    candidates.plans.each do |plan|
+        puts "# Begin of plan ####"
+        plan.action_sequence.each do |action|
+            puts "#{action.name}"
+            action.arguments.each do |arg|
+                puts "\t#{arg}"
+            end
+        end
+        puts "# End of plan ##### "
+    end
+
+end
